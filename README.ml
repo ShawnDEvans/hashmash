5cec00581707bb4565152802d4e06e324734b087
Shawn Evans <ShawnDEvans@gmail.com>
2015-05-20 22:08:43 -0400

    Initial commit

---
+++ README.ml
@@ -0,0 +1,6 @@
+HashMash - decrypted password to username matcher
+
+$ python hashmash.py <JTR Hash File> <OCL Hashcat Decrypted File>
+
+User Hash File format is user:hash (or JTR NTLM)
+OCL Decrypted Pasword File format is, hash:password
